3242c639b56d24d1592d0ac6d334d3d90de783f5
Akshay Anithan <akshayanithan@gmail.com>
2025-08-21 20:20:17 +0530

    React Series | MovieHub first draft

---
+++ MovieHub/src/pages/Favorites.jsx
@@ -0,0 +1,32 @@
+import "../css/Favorites.css";
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
+
+function Favorites() {
+  const { favorites } = useMovieContext();
+
+  if (favorites.length > 0) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id}></MovieCard>
+          ))}
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="favorites-empty">
+        <h1>No Favorite Movies Yet</h1>
+        <p>
+          Start adding some movies to your favorites and they will appear
+          here...
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Favorites;
